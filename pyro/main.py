d6c552b2d2bca3ebe47850bed7e20462fa02035e
Callum John <b6010572@my.shu.ac.uk>
2018-10-09 20:25:00 +0100

    Allowed pyro to deal with project folders that aren't empty

--- pyro/main.py
+++ pyro/main.py
@@ -1,6 +1,7 @@
 import argparse
 import sys
-from utils.file import file_get_contents
+import os
+from utils.file import *
 
 def main():
 	# Create a new ArgumentParser
@@ -22,8 +23,32 @@ def main():
 		parser.print_help()
 		parser.exit()
 
+	# React accordingly based on which argument they used
 	if args.new:
-		print(args.new)
+		if os.path.exists(args.new):
+			source = 'CodeIgniter-3.1.9'
+			destination = args.new
+			choice = ''
+
+			# Warn the user that the project's folder isn't empty
+			# and give them the option of cancelling
+			while choice != 'Y' and choice != 'n':
+				choice = input('WARNING: Folder isn\'t empty. Overwrite? (Y/n):')
+
+				if choice != 'Y' and choice != 'n':
+					print('ERROR: Invalid choice. Enter only \'Y\' or \'n\'')
+
+			if choice == 'Y':
+				copy_tree(source, destination)
+		else:
+			# TODO: Make this sensitive to the version number
+			source = 'CodeIgniter-3.1.9'
+			destination = args.new
+
+			os.makedirs(args.new)
+			copy_tree(source, destination)
+
+		print('Done.')
 
 if __name__ == '__main__':
 	main()
--- pyro/utils/file.py
+++ pyro/utils/file.py
@@ -1,5 +1,28 @@
+import os
+import shutil
+
 def file_get_contents(path):
 	with open(path, 'r') as file:
 		contents = file.read()
 
 	return contents
+
+# Modified from this StackOverflow post:
+# https://stackoverflow.com/questions/1868714/how-do-i-copy-an-entire-directory-of-files-into-an-existing-directory-using-pyth#12514470
+def copy_tree(source, destination, symlinks = False, ignore = None):
+	for item in os.listdir(source):
+		s = os.path.join(source, item)
+		d = os.path.join(destination, item)
+
+		print('Copying', s, 'to', d, '...')
+
+		if os.path.isdir(s):
+			if os.path.exists(d):
+				shutil.rmtree(d)
+
+			shutil.copytree(s, d, symlinks, ignore)
+		else:
+			if os.path.exists(d):
+				os.remove(d)
+
+			shutil.copy2(s, d)
