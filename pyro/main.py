f3142287ce014c1c2c07bf427aaa02a1a8804950
Callum John <b6010572@my.shu.ac.uk>
2018-10-09 19:05:32 +0100

    Added new argument

--- pyro/main.py
+++ pyro/main.py
@@ -1,13 +1,29 @@
 import argparse
 import sys
-import utils.file
+from utils.file import file_get_contents
 
 def main():
+	# Create a new ArgumentParser
 	description = file_get_contents('config/description.txt')
 	parser = argparse.ArgumentParser(description = description)
+	
+	# Add the command line arguments
+	parser.add_argument(
+		'-n',
+		'--new',
+		help = 'Creates a new CodeIgniter project with the given name'
+	)
+
+	# Parse the arguments sent by the user and store them
 	args = parser.parse_args()
 
-	if sys.args
+	# By default, show the help message
+	if len(sys.argv[1:]) == 0:
+		parser.print_help()
+		parser.exit()
+
+	if args.new:
+		print(args.new)
 
 if __name__ == '__main__':
 	main()
--- pyro/utils/file.py
+++ pyro/utils/file.py
@@ -1,8 +1,5 @@
-import shutil
-
-'''Obtains the contents of a text file as a Python string.'''
 def file_get_contents(path):
-	with open(path, r) as file:
+	with open(path, 'r') as file:
 		contents = file.read()
 
 	return contents
