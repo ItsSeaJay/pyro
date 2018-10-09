d4b22f3c9a98d492470922e586a15df29a36df93
Callum John <b6010572@my.shu.ac.uk>
2018-10-09 17:07:22 +0100

    Accidentally forgot to checkout develop

--- pyro/main.py
+++ pyro/main.py
@@ -1,2 +1,13 @@
+import argparse
+import sys
+import utils.file
+
+def main():
+	description = file_get_contents('config/description.txt')
+	parser = argparse.ArgumentParser(description = description)
+	args = parser.parse_args()
+
+	if sys.args
+
 if __name__ == '__main__':
-	print('Hello, World!')
+	main()
